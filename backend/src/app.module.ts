8e4b5ef35cf74149a5c5e78f0ce851f79897c41e
Vivvek09 <130150195+Vivvek09@users.noreply.github.com>
2024-08-29 12:33:56 +0530

    mongodb setup (#2)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module'; // Assuming you have a UserModule
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://vivekagangwani:EPiUJaC7dSOhQDoR@cluster0.skc7t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'), // Replace with your MongoDB connection string
+    UserModule, 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
