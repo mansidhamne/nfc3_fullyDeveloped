ad8588d18383e0501fe2a92e030719d0423d1612
Vivek Gangwani <vivekagangwani@gmail.com>
2024-08-30 12:30:15 +0530

    Deploy Ready

--- frontend/app/lectures/page.tsx
+++ frontend/app/lectures/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
         <div className="flex flex-col min-w-full min-h-full bg-white py-8 pl-4 pr-2" >
             <div className="w-full">
             <h1 className="text-3xl text-black font-bold ">Lecture Attendance Manager</h1>
-            <h5> className='text-xl text-indigo-500 font-medium mt-1'Start Attendance, View Attendees Real Time and Generate Report!</h5>
+            <h5 className='text-xl text-indigo-500 font-medium mt-1'> Start Attendance, View Attendees Real Time and Generate Report!</h5>
              <div className='max-w-1/2' style={{maxWidth:'80vw'}}>
              <LectureAttendance/>
              </div>
